1f421eec2376670d82b2333a6e9f9366bdd709a8
Catherine Johnson <cfjohnson@earthlink.net>
2018-09-25 21:33:57 -0600

    made update in master

--- python_code_kata/arrays_and_strings/substring_w_max_k_chars.py
+++ python_code_kata/arrays_and_strings/substring_w_max_k_chars.py
@@ -3,3 +3,5 @@
 # ex1. Input: s = "eceba", k = 2  Output: 3  Explanation: T is "ece" which its length is 3.
 
 # ex2. Input: s = "aa", k = 1  Output: 2  Explanation: T is "aa" which its length is 2.
+
+# test with examples and explain optimization.
